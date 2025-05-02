6ff0d537e9f2c2babcdfeb359caf16b5f42ccd6f
wuyuanwuhui99 <275018723@qq.com>
2025-05-02 12:28:49 +0800

    修改pom文件

---
+++ 一键提交到gitee.bat
@@ -0,0 +1,4 @@
+git remote rm origin
+git remote add origin https://gitee.com/wuyuanwuhui99/springboot3-app-service
+git push origin main
+pause
---
+++ 一键提交到github.bat
@@ -0,0 +1,4 @@
+git remote rm origin
+git remote add origin https://github.com/wuyuanwuhui999/springboot3-app-service.git
+git push origin main
+pause
