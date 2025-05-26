98a3d270a1fa2b69b20ae2dc411cdf7a2948ccad
wuyuanwuhui99 <275018723@qq.com>
2025-05-26 21:13:25 +0800

    优化系统提示词

--- ai/src/main/java/com/player/ai/config/ChatClientConfig.java
+++ ai/src/main/java/com/player/ai/config/ChatClientConfig.java
@@ -1,5 +1,6 @@
 package com.player.ai.config;
 
+import com.player.ai.constants.SystemtConstants;
 import com.player.ai.tool.MusicTool;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
@@ -20,16 +21,16 @@ public class ChatClientConfig {
     }
 
     @Bean
-    public ChatClient chatClient(OllamaChatModel model, ChatMemory chatMemory) {
+    public ChatClient chatClient(OllamaChatModel model, ChatMemory chatMemory,MusicTool musicTool) {
         return ChatClient
                 .builder(model)
                 .defaultOptions(ChatOptions.builder().model("qwen3:8b").build())
-                .defaultSystem("你是一个热心、可爱的智能音乐助手，你的名字叫小吴同学，请以小吴同学的身份和语气回答问题。")
+                .defaultSystem(SystemtConstants.MUSIC_SYSTEMT_PROMPT)
                 .defaultAdvisors(
                         new SimpleLoggerAdvisor(),
                         new MessageChatMemoryAdvisor(chatMemory)
                 )
-                .defaultTools(new MusicTool())
+                .defaultTools(musicTool)
                 .build();
     }
 }
