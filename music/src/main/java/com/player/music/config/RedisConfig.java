21d779a0c325014014c1d7a2d40734c72ca85659
wuyuanwuhui99 <275018723@qq.com>
2025-06-29 09:12:47 +0800

    修改redis配置问题

--- music/src/main/java/com/player/music/config/RedisConfig.java
+++ music/src/main/java/com/player/music/config/RedisConfig.java
@@ -1,4 +1,7 @@
 package com.player.music.config;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
 import org.springframework.ai.chat.messages.Message;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -37,15 +40,21 @@ public class RedisConfig {
         }
     }
 
-    // 通用 RedisTemplate
     @Bean
-    public RedisTemplate<String, Message> redisTemplate(RedisConnectionFactory factory) {
-        RedisTemplate<String, Message> template = new RedisTemplate<>();
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(factory);
         template.setKeySerializer(new StringRedisSerializer());
 
-        // 使用专门的 Jackson 序列化器配置
-        Jackson2JsonRedisSerializer<Message> serializer = new Jackson2JsonRedisSerializer<>(Message.class);
+        // Configure polymorphic serialization
+        PolymorphicTypeValidator ptv = BasicPolymorphicTypeValidator.builder()
+                .allowIfSubType("org.springframework.ai.chat.messages")
+                .build();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.activateDefaultTyping(ptv, ObjectMapper.DefaultTyping.NON_FINAL);
+
+        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
         template.setValueSerializer(serializer);
 
         return template;
@@ -53,7 +62,7 @@ public class RedisConfig {
 
     // 专门用于 Message 类型的 RedisTemplate
     @Bean
-    public RedisTemplate<String, Message> messageRedisTemplate(
+    public RedisTemplate<String,Message> messageRedisTemplate(
             RedisConnectionFactory factory
     ) {
         RedisTemplate<String, Message> template = new RedisTemplate<>();
